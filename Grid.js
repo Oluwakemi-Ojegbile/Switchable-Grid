39e1656943b362cae66d0a325ae8f8270b035344
unknown <raimioluwakemi@yahoo.com>
2019-04-08 18:19:58 +0100

    first commit

---
+++ Grid.js
@@ -0,0 +1,57 @@
+new Vue({
+    el: '#main-wrapper',
+    data: {
+       
+        layout: 'grid',
+
+        articles: [{
+            "title": "Pauses For Lent: Who Do You Say I Am?",
+            "url": "https://renovare.org/articles/pauses-for-lent-who-do-you-say-i-am",
+            "image": {
+                "large": "https://renovare.imgix.net/resources/Pauses-for-Lent.jpg?h=600",
+                "small": "https://renovare.imgix.net/resources/Pauses-for-Lent.jpg?h=600"
+            }
+        },
+        {
+            "title": "What to Pray When Pain Takes Your Words",
+            "url": "https://renovare.org/articles/what-to-pray-when-pain-takes-your-words",
+            "image": {
+                "large": "https://renovare.imgix.net/articles/man-kneeling-praying-on-pew.jpg?w=900&q=85",
+                "small": "https://renovare.imgix.net/articles/man-kneeling-praying-on-pew.jpg?w=900&q=85"
+            }
+        },
+        {
+            "title": "The Blessedness of Possessing Nothing",
+            "url": "https://renovare.org/articles/blessedness-possessing-nothing",
+            "image": {
+                "large": "https://renovare.imgix.net/articles/12.07-Tyranny-Things.jpg?w=900&q=85",
+                "small": "https://renovare.imgix.net/articles/12.07-Tyranny-Things.jpg?w=900&q=85"
+            }
+        },
+        {
+            "title": "The Secret of Guidance",
+            "url": "https://renovare.org/articles/the-secret-of-guidance",
+            "image": {
+                "large": "https://renovare.imgix.net/articles/07.22-Fireflies-as-Lantern.jpg?w=900&q=85",
+                "small": "https://renovare.imgix.net/articles/07.22-Fireflies-as-Lantern.jpg?w=900&q=85"
+            }
+        },
+        {
+            "title": "An Habitual Sense of His Presence",
+            "url": "https://renovare.org/articles/an-habitual-sense-of-gods-presence",
+            "image": {
+                "large": "https://renovare.imgix.net/articles/lightstock_147649_medium_user_1712940.jpg?w=900&q=85",
+                "small": "https://renovare.imgix.net/articles/lightstock_147649_medium_user_1712940.jpg?w=900&q=85"
+            }
+        },
+        {
+            "title": "Acquiring a Taste for God",
+            "url": "https://renovare.org/articles/acquiring-a-taste-for-god",
+            "image": {
+                "large": "https://renovare.imgix.net/articles/Cup.jpeg?w=900&q=85",
+                "small": "https://renovare.imgix.net/articles/Cup.jpeg?w=900&q=85"
+            }
+        }]
+
+    }
+});
